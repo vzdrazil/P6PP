477202a401e579be00c32f0b42c6ba228ff212e3
polid <polid@PC-DAN>
2025-03-13 22:08:33 +0100

    Add entities for template

---
+++ NotificationService.API/Persistence/Entities/DB/Interfaces/Template.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace NotificationService.API.Persistence.Entities.DB.Interfaces;
+
+public class Template
+{
+    [StringLength(100)]
+    public required string Name { get; set; }
+    [StringLength(75)]
+    public string? Subject { get; set; }
+    //Budou vytvoreny zastupne znaky pro vlozeni promennych(jmeno,datum,atd.)
+    [StringLength(1500)]
+    public required string Text { get; set; }
+    [ForeignKey(nameof(TemplateType))]
+    public required int TypeId { get; set; }
+}
---
+++ NotificationService.API/Persistence/Entities/DB/Interfaces/TemplateType.cs
@@ -0,0 +1,8 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace NotificationService.API.Persistence.Entities.DB.Interfaces;
+
+public class TemplateType
+{
+    [StringLength(50)]
+    public required string Name { get; set; }
+}
