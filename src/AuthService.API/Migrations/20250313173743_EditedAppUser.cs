4c3e54fdf3fb88294131e4d93f969b4838ec647f
liborvyleta <libor.vyleta@icloud.com>
2025-03-13 21:41:06 +0100

    add migrations

---
+++ src/AuthService.API/Migrations/20250313173743_EditedAppUser.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AuthService.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class EditedAppUser : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<double>(
+                name: "Weight",
+                table: "AspNetUsers",
+                type: "double",
+                nullable: true,
+                oldClrType: typeof(double),
+                oldType: "double");
+
+            migrationBuilder.AlterColumn<double>(
+                name: "Height",
+                table: "AspNetUsers",
+                type: "double",
+                nullable: true,
+                oldClrType: typeof(double),
+                oldType: "double");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<double>(
+                name: "Weight",
+                table: "AspNetUsers",
+                type: "double",
+                nullable: false,
+                defaultValue: 0.0,
+                oldClrType: typeof(double),
+                oldType: "double",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<double>(
+                name: "Height",
+                table: "AspNetUsers",
+                type: "double",
+                nullable: false,
+                defaultValue: 0.0,
+                oldClrType: typeof(double),
+                oldType: "double",
+                oldNullable: true);
+        }
+    }
+}
