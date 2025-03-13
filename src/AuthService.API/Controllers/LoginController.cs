a9b7ca0922455add2560b6393b6df24021f89ad7
liborvyleta <libor.vyleta@icloud.com>
2025-03-13 16:32:00 +0100

    login added

---
+++ src/AuthService.API/Controllers/LoginController.cs
@@ -0,0 +1,76 @@
+using AuthService.API.DTOs;
+using AuthService.API.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace AuthService.API.Controllers
+{
+    [Route("api/login")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly IConfiguration _configuration;
+
+        public LoginController(UserManager<ApplicationUser> userManager, IConfiguration configuration)
+        {
+            _userManager = userManager;
+            _configuration = configuration;
+        }
+
+        [HttpPost("authenticate")]
+        public async Task<IActionResult> Login([FromBody] LoginModel model)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest("Invalid data.");
+
+            // Find user by email
+            var user = await _userManager.FindByEmailAsync(model.Email);
+            if (user == null)
+                return Unauthorized("Invalid email or password.");
+
+            // Check if the password is correct
+            var result = await _userManager.CheckPasswordAsync(user, model.Password);
+            if (!result)
+                return Unauthorized("Invalid email or password.");
+
+            // Create the JWT token
+            var token = GenerateJwtToken(user);
+
+            return Ok(new { token });
+        }
+
+        private string GenerateJwtToken(ApplicationUser user)
+        {
+            var claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.NameIdentifier, user.Id),
+                new Claim(ClaimTypes.Name, user.UserName),
+                new Claim(ClaimTypes.Email, user.Email),
+            };
+
+            // Přidání role jako claim, pokud role není null
+            if (user.Role != null && !string.IsNullOrEmpty(user.Role.Name))
+            {
+                claims.Add(new Claim("role", user.Role.Name));
+            }
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Jwt:Key"]));
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(
+                issuer: _configuration["Jwt:Issuer"],
+                audience: _configuration["Jwt:Audience"],
+                claims: claims,
+                expires: DateTime.Now.AddMinutes(60),
+                signingCredentials: creds
+            );
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+    }
+}
