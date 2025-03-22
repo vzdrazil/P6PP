4d97604c424d4be4f420cab2566c021cf4ac8c59
fr0g1299 <dodoloka1299@gmail.com>
2025-03-22 19:45:02 +0100

    Added Template service

---
+++ src/NotificationService.API/Services/TemplateAppService.cs
@@ -0,0 +1,30 @@
+﻿using NotificationService.API.Persistence.Entities.DB;
+using NotificationService.API.Persistence.Entities.DB.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace NotificationService.API.Services
+{
+    public class TemplateAppService
+    {
+        private readonly NotificationDbContext _notificationDbContext;
+
+        public TemplateAppService(NotificationDbContext notificationDbContext)
+        {
+            _notificationDbContext = notificationDbContext;
+        }
+
+        public async Task<Template?> SendTemplateAsync(string name, string language = "cs")
+        {
+            var template = await _notificationDbContext.Templates
+                .FirstOrDefaultAsync(x => x.Name == name && x.Language == language);
+
+            return template;
+        }
+
+        public async Task EditTemplateAsync(Template template)
+        {
+            _notificationDbContext.Templates.Update(template);
+            await _notificationDbContext.SaveChangesAsync();
+        }
+    }
+}
