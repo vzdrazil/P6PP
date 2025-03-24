b5568871a5d210b3a49cddafba1a1835390da7b4
apolkova <a_polkova@utb.cz>
2025-03-24 11:39:07 +0100

    updating entity Rooms and adding RoomStatus

---
+++ src/BookingPayments.API/Entities/RoomStatus.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BookingPayments.API.Entities
+{
+    [Table(nameof(RoomStatus))]
+    public class RoomStatus : Entity<int>
+    {
+        public string? Status { get; set; }
+    }
+}
