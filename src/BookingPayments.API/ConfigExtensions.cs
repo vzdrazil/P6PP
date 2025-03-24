bc6d5f9d0c1741dee89492afedc19278dd66e6e3
filip <filip.chromecka@outlook.com>
2025-03-24 22:56:41 +0100

    Add scrutor

---
+++ src/BookingPayments.API/ConfigExtensions.cs
@@ -0,0 +1,17 @@
+﻿using BookingPayments.API.Application.Abstraction;
+
+namespace BookingPayments.API;
+
+internal static class ConfigExtensions
+{
+    public static IServiceCollection AddServices(this IServiceCollection services)
+    {
+        services.Scan(scan => scan
+            .FromAssemblyOf<IRoomAppService>()
+            .AddClasses(classes => classes.Where(type => type.Name.EndsWith("Service")))
+            .AsImplementedInterfaces()
+            .WithScopedLifetime());
+
+        return services;
+    }
+}
