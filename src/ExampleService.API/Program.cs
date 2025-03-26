bb6980b72b8c842732d2a37c28eb28ef4ce982b7
Jiri Drapal <41378892+JustJirka@users.noreply.github.com>
2025-03-26 12:54:11 +0100

    reveversed commit ff8f128f by polida

--- src/ExampleService.API/Program.cs
+++ src/ExampleService.API/Program.cs
@@ -10,7 +10,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Configure port here + launchSettings.json ( + later Dockerfile EXPOSE XXXX)
 builder.WebHost.ConfigureKestrel(options =>
 {
-    options.ListenAnyIP(5187);
+    options.ListenAnyIP(5188);
 });
 
 builder.Services.AddEndpointsApiExplorer();
